c313097cb47fcdd1e636df14c4c2a7eaab994ca4
zhufengnodejs <zhufengnodejs@126.com>
2016-08-27 13:18:16 +0800

    如果用户名被占用，则注册失败

--- routes/user.js
+++ routes/user.js
@@ -13,25 +13,34 @@ router.post('/reg', function(req, res, next) {
   if(user.password != user.repassword){
     return res.redirect('back');
   }
-  //1. 对密码进行md5加密
-  user.password = utils.md5(user.password);
-  //2. 通过邮箱生成头像
-  user.avatar = "https://secure.gravatar.com/avatar/"+utils.md5(user.email)+"?s=28";
-  //通过User可以得到模型对象
-  Model('User').create(user,function(err,doc){
-    //err 错误对象 doc 是保存成功后的对象
-    if(err){
-      req.flash('error','很抱歉,你注册失败'+err);
+  Model('User').findOne({username:user.username},function(err,result){
+    if(result){
+      req.flash('error','很抱歉,你的用户名已经被人占用，注册失败');
       //如果失败则滚回到上个页面重新填写
       return res.redirect('back');
     }else{
-      req.flash('success','恭喜你注册成功')
-      //如果保存成功了，则把保存后的文档对象写入当前的session中
-      req.session.user  = doc;
-      //重定向到首页
-      return res.redirect('/');
+      //1. 对密码进行md5加密
+      user.password = utils.md5(user.password);
+      //2. 通过邮箱生成头像
+      user.avatar = "https://secure.gravatar.com/avatar/"+utils.md5(user.email)+"?s=28";
+      //通过User可以得到模型对象
+      Model('User').create(user,function(err,doc){
+        //err 错误对象 doc 是保存成功后的对象
+        if(err){
+          req.flash('error','很抱歉,你注册失败'+err);
+          //如果失败则滚回到上个页面重新填写
+          return res.redirect('back');
+        }else{
+          req.flash('success','恭喜你注册成功')
+          //如果保存成功了，则把保存后的文档对象写入当前的session中
+          req.session.user  = doc;
+          //重定向到首页
+          return res.redirect('/');
+        }
+      });
     }
   });
+
 });
 //不是完整的路径，而是/users后面的路径
 //用户登陆
