d863e75cb57fb4f2ce3bc803e386647ca0febcde
zhufengnodejs <zhufengnodejs@126.com>
2016-08-28 10:28:47 +0800

    实现了编辑功能

--- routes/article.js
+++ routes/article.js
@@ -28,24 +28,40 @@ router.get('/list',auth.mustLogin,function(req,res){
 
 //显示增加文章的表单路由
 router.get('/add',auth.mustLogin,function(req,res){
-    res.render('article/add');
+    //新增加文章的时候article是空对象
+    res.render('article/add',{title:'新增文章',article:{}});
 });
 
 router.post('/add',auth.mustLogin,function(req,res){
     //从请求体中得到文档对象
     var article = req.body;
-    //从会话对象session中取出userId,赋给文章的发表人字段
-    article.user = req.session.user._id;
-    //把此文件存放到数据库里
-    Model('Article').create(article,function(err,doc){
-        if(err){
-            req.flash('error','文章发表失败了');
-            res.redirect('back');
-        }else{
-            req.flash('success','文章发表成功');
-            res.redirect('/');
-        }
-    });
+    var articleId = article._id;
+    if(articleId){//如果ID有值表示修改文章
+        Model('Article').update({_id:articleId},{$set:{
+            title:article.title,//能且只能更新title和content两个字段
+            content:article.content//指定修改后的内容
+        }}).then(function(result){
+            res.redirect('/article/detail/'+articleId);
+        });
+    }else{//如果ID没有值表示新增加文章
+          //从会话对象session中取出userId,赋给文章的发表人字段
+        article.user = req.session.user._id;
+        //因为新增的时候_id传过来的是空字符串，因为_id主键不能为空,所以要删除此字段
+        // 因为如果_id不值的话,mongodb会直接保存
+        //如果_id为null,mongodb会帮我们自动生成一个合法_id值
+        delete article._id;
+        //把此文件存放到数据库里
+        Model('Article').create(article,function(err,doc){
+            if(err){
+                req.flash('error','文章发表失败了');
+                res.redirect('back');
+            }else{
+                req.flash('success','文章发表成功');
+                res.redirect('/');
+            }
+        });
+    }
+
 });
 
 router.get('/detail/:articleId',function(req,res){
@@ -65,10 +81,23 @@ router.get('/delete/:articleId',function(req,res){
   //删除指定的文章
   Model('Article').remove({_id:articleId}).then(function(data){
       req.flash('success','删除文章成功');
-      req.redirect('/');
-  }).catch(function(error){
+      res.redirect('/');
+  },function(error){
       req.flash('error','删除文章失败');
-      req.redirect('back');
+      res.redirect('back');
   });
+});
+/**
+ * 1. 点击编辑链接跳到增加文章的页面，并且回显文章原来的内容
+ * 2. 进行修改编辑，修改完成后保存文章
+ */
+router.get('/update/:articleId',function(req,res){
+    //先得到要编辑的文章ID
+    var articleId = req.params.articleId;
+    //根据文章的ID查询文章的文档对象
+    Model('Article').findById(articleId).then(function(doc){
+        //渲染模板，并传递当前的文章对象
+        res.render('article/add',{title:'编辑文章',article:doc});
+    })
 });
 module.exports = router;
